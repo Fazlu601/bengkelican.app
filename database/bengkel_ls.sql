8ee22fc7aeea4ae3cb9db750044374dec793360b
Fazlu601 <fazrlu9575@gmail.com>
2023-06-19 23:53:12 +0700

    Change database sql file

--- database/bengkel_ls.sql
+++ database/bengkel_ls.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2023 at 08:12 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Jun 19, 2023 at 06:51 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `failed_jobs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -45,9 +45,9 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `karyawans` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telephone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -57,7 +57,7 @@ CREATE TABLE `karyawans` (
 --
 
 INSERT INTO `karyawans` (`id`, `name`, `address`, `telephone`, `created_at`, `updated_at`) VALUES
-(1, 'icipp', 'Talang Bakung', '082252879012', '2023-03-14 11:35:49', '2023-03-14 11:35:49');
+(1, 'icipp', 'Talang Bakung', '082252879012', '2023-06-15 09:30:59', '2023-06-15 09:30:59');
 
 -- --------------------------------------------------------
 
@@ -67,7 +67,7 @@ INSERT INTO `karyawans` (`id`, `name`, `address`, `telephone`, `created_at`, `up
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -82,7 +82,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2023_03_14_154013_pelanggan', 1),
 (6, '2023_03_14_163357_karyawan', 1),
-(7, '2023_03_14_173215_product', 1);
+(7, '2023_03_14_173215_product', 1),
+(8, '2023_03_14_191845_shop_info', 1),
+(9, '2023_03_15_085432_transaction', 1);
 
 -- --------------------------------------------------------
 
@@ -91,8 +93,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -104,10 +106,10 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `pelanggans` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telephone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -117,7 +119,7 @@ CREATE TABLE `pelanggans` (
 --
 
 INSERT INTO `pelanggans` (`id`, `name`, `address`, `telephone`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Alex', 'Jl. Abdul Muis RT. 15 Jeramba Bolong', '6285266940312', NULL, '2023-03-14 11:35:49', '2023-03-14 11:35:49');
+(1, 'Alex', 'Jl. Abdul Muis RT. 15 Jeramba Bolong', '6285266940312', NULL, '2023-06-15 09:30:59', '2023-06-15 09:30:59');
 
 -- --------------------------------------------------------
 
@@ -127,11 +129,11 @@ INSERT INTO `pelanggans` (`id`, `name`, `address`, `telephone`, `remember_token`
 
 CREATE TABLE `personal_access_tokens` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -145,10 +147,10 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `products` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `type` enum('service','sparepart') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `stock` int(11) NOT NULL,
+  `type` enum('service','sparepart') NOT NULL,
+  `stock` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -158,8 +160,54 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `type`, `stock`, `created_at`, `updated_at`) VALUES
-(1, 'Velg Merah Avanza 2011', 300000, 'sparepart', 8, '2023-03-14 11:35:49', '2023-03-14 11:35:49'),
-(2, 'Velg Merah Avanza 2011', 300000, 'service', 10, '2023-03-14 11:35:49', '2023-03-14 11:35:49');
+(1, 'Velg Merah Avanza 2011', 300000, 'sparepart', 8, '2023-06-15 09:30:59', '2023-06-15 09:30:59'),
+(2, 'Cat Bamper Depan & Belakang', 620000, 'service', NULL, '2023-06-15 09:30:59', '2023-06-15 09:30:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop_infos`
+--
+
+CREATE TABLE `shop_infos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `shop_infos`
+--
+
+INSERT INTO `shop_infos` (`id`, `name`, `address`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'BENGKEL MOBIL ICAN', 'Jl. Abdul Chatab RT.20 Kel.Pasir', NULL, '2023-06-15 09:30:59', '2023-06-15 09:30:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `type` enum('service','sparepart') NOT NULL,
+  `total` int(11) NOT NULL,
+  `supplier` varchar(255) NOT NULL,
+  `plat` varchar(255) NOT NULL,
+  `kondisi` enum('sedang diproses','sudah sampai') NOT NULL DEFAULT 'sedang diproses',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `type`, `total`, `supplier`, `plat`, `kondisi`, `created_at`, `updated_at`) VALUES
+(5, 'sparepart', 100000, 'Riyan Setiyawan', 'BH 1805 PGI', 'sudah sampai', '2023-06-19 07:59:02', '2023-06-19 08:58:45');
 
 -- --------------------------------------------------------
 
@@ -169,14 +217,14 @@ INSERT INTO `products` (`id`, `name`, `price`, `type`, `stock`, `created_at`, `u
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) NOT NULL,
   `is_admin` tinyint(1) NOT NULL DEFAULT 0,
   `is_supplier` tinyint(1) NOT NULL DEFAULT 0,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -186,7 +234,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `name`, `address`, `email_verified_at`, `password`, `is_admin`, `is_supplier`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Fazlu601', 'Fazlu Rachman', 'Kenali Asam Bawah', NULL, '$2y$10$csy1uHRLqgIjdfjv71M7ZOCOw7YyeHACQq76T3nJunacSGt2Lnvim', 0, 0, NULL, '2023-03-14 11:35:49', '2023-03-14 11:35:49');
+(1, 'Daffa01', 'Daffa Darmawan', 'Kenali Asam Bawah', NULL, '$2y$10$2o3x0hChzCWj6FOco.Be2uL1ROP69sh18eKjP7kZcQvSVWS/LCi5O', 1, 0, NULL, '2023-06-15 09:30:59', '2023-06-15 09:30:59'),
+(5, 'Rahmat03', 'Rahmat Pratama Illahi', 'Arizonaaaa', NULL, '$2y$10$5bwA51.zAtxCodoc4CfkneWG/FAnSIBdFIL0UtBMWzqOejxBXXGxG', 0, 1, NULL, '2023-06-19 07:06:20', '2023-06-19 07:06:20'),
+(7, 'Riyan123', 'Riyan Setiyawan', 'Arizonaaaa', NULL, '$2y$10$fINgh3wbTqTTlcXwqdSUe.hBy8QDV.fO1g5pGhv95ETwAU.kqG6Ee', 0, 1, NULL, '2023-06-19 07:58:35', '2023-06-19 07:58:35');
 
 --
 -- Indexes for dumped tables
@@ -238,6 +288,18 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `shop_infos`
+--
+ALTER TABLE `shop_infos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -264,7 +326,7 @@ ALTER TABLE `karyawans`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `pelanggans`
@@ -285,10 +347,22 @@ ALTER TABLE `products`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `shop_infos`
+--
+ALTER TABLE `shop_infos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
